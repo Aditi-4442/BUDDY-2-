1d09d88c6b04829a284eb01b9943892e19efc803
Aditi-4442 <aduaditisingh113@gmail.com>
2025-05-23 10:51:08 +0530

    Create Basic.java

---
+++ Basic.java
@@ -0,0 +1,16 @@
+java
+Copy code
+public class Person {
+    protected String name;
+    protected String email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Email: " + email);
+    }
+}
